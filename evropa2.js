4c912363f5bf8f4f592e3e8ea549e39fe999a1c1
Lenka Koubková <lvseteckova@gmail.com>
2025-04-21 15:02:10 +0200

    úprava html evropa2

--- evropa2.js
+++ evropa2.js
@@ -4,18 +4,5 @@ import {renderStation, renderNavigation } from "./radio.js";
   renderNavigation()
   renderStation(0); //Zobrazíme první stanici
 
-  //funkce pro navigaci
-
-  export const renderNavigation = () => {    //funkce renderNavigation, slovo export umožňuje, aby byla funkce dostupná i v jiných souborech
-    const header = document.querySelector("header"); //hledá HTML prvek header na stránce, to je místo 
-    const links = []; // Vytvoříme prázdný seznam odkazů
-    // Pro každou stanici vytvoříme odkaz a přidáme ho do seznamu
-    radio.forEach(station => {
-    const link = `<a href="${station.link}">${station.name}</a>`;
-    links.push(link);
-  });
-   // Spojíme odkazy pomocí " | " a přidáme je do hlavičky
-   header.innerHTML = links.join(' | ');
-  };
   
   
